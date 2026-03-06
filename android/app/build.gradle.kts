dfda5f2269b897eeaa3fcf495ad21ed236bd2140
SLICKMAN-TYRUS <a.chol@alustudent.com>
2026-03-06 21:54:38 +0300

    chore: Configure Firebase for Android - Add Google services Gradle plugin v4.4.4 to settings.gradle.kts - Apply google-services plugin in app-level build.gradle.kts - Add Firebase BoM v34.10.0 with Analytics, Auth, and Firestore dependencies - Fix package name to com.slickman_tyrus.kigali

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Firebase plugin
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.slickman_tyrus.kigali.kigali_city_navigator"
+    namespace = "com.slickman_tyrus.kigali"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,11 +21,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.slickman_tyrus.kigali.kigali_city_navigator"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        applicationId = "com.slickman_tyrus.kigali"
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +37,19 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// Firebase dependencies - using versions from your screenshot
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
+    
+    // Firebase Analytics
+    implementation("com.google.firebase:firebase-analytics")
+    
+    // Firebase Auth
+    implementation("com.google.firebase:firebase-auth")
+    
+    // Firestore
+    implementation("com.google.firebase:firebase-firestore")
 }
